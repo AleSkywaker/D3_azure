d6546463506d6efa74a1d01bb1083b3c7d66dd95
Alex Colombo <uricaine@hotmail.com>
2019-07-12 15:23:53 +0200

    lesson 2

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const data = [{ width: 200, height: 200, fill: 'purple' }];
 
 const svg = d3.select('svg');
 // append shapes to svg container
-svg
-  .append('rect')
-  .attr('width', 30)
+const rect = svg
+  .select('rect')
+  .data(data)
+  .attr('width', function(d) {
+    console.log(d.width);
+  })
   .attr('height', 80)
   .attr('fill', 'teal')
   .attr('x', 10)
   .attr('y', 200 - 80);
+
+// console.log(rect);
