cc70985b7be16919613f2939295fc11a153e9b67
Alex Colombo <uricaine@hotmail.com>
2019-07-13 11:44:05 +0200

    lesson3 arrow function

--- app.js
+++ app.js
@@ -5,14 +5,8 @@ const svg = d3.select('svg');
 const rect = svg
   .select('rect')
   .data(data)
-  .attr('width', function(d, i, n) {
-    return d.width;
-  })
-  .attr('height', function(d) {
-    return d.height;
-  })
-  .attr('fill', function(d) {
-    return d.fill;
-  })
+  .attr('width', (d, i, n) => d.width)
+  .attr('height', d => d.height)
+  .attr('fill', d => d.fill)
   .attr('x', 10)
   .attr('y', 200 - 80);
