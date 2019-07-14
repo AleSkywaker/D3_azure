9b512a0d3736ed74d391f4fb50096448c77b9f1d
Alex Colombo <uricaine@hotmail.com>
2019-07-14 10:34:11 +0200

    start lesson8

--- app.js
+++ app.js
@@ -3,27 +3,8 @@
 //Seleccionamos el elemento svg
 const svg = d3.select('svg');
 
-d3.json('planetas.json').then(datos => {
+d3.json('menu.json').then(datos => {
   //Añadimos lo anterior a un grupo
-  const svg = d3.select('svg');
-  const grupo = svg.append('g').attr('transform', 'translate(25,0)');
-  const circulos = grupo.selectAll('circle').data(datos);
-
-  //Añadimos circulos en elementos existentes en DOM
-  circulos
-    .attr('cy', 100)
-    .attr('cx', d => d.distance)
-    .attr('r', d => d.radius)
-    .attr('fill', d => d.fill);
-
-  // Añadimos elementos on the fly
-  circulos
-    .enter()
-    .append('circle')
-    .attr('r', d => d.radius)
-    .attr('cx', d => d.distance)
-    .attr('cy', 100)
-    .attr('fill', d => d.fill);
 
   console.log(circulos);
 });
