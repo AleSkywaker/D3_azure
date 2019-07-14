06f706bf3ce3c41912fb883b98132b1d9844aa86
Alex Colombo <uricaine@hotmail.com>
2019-07-14 11:24:58 +0200

    start lesson8

--- app.js
+++ app.js
@@ -2,10 +2,24 @@
 
 //Seleccionamos el elemento svg
 const svg = d3.select('svg');
-const grupo = svg.append('g');
 
 d3.json('edificios.json').then(datos => {
   //Añadimos ldatos a los rectangulos
-  const rects = grupo.selectAll('rects').data(datos);
+  const rects = svg.selectAll('rect').data(datos);
+
+  rects
+    .attr('width', 20)
+    .attr('height', d => d.altura)
+    .attr('fill', 'crimson')
+    .attr('x', (d, i) => i * 30);
+
+  rects
+    .enter()
+    .append('rect')
+    .attr('width', 20)
+    .attr('height', d => d.altura)
+    .attr('fill', 'crimson')
+    .attr('x', (d, i) => i * 30);
+
   console.log(datos);
 });
