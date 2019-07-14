076ec1d0cd4ded75c9dfe082e0a65104da66b956
Alex Colombo <uricaine@hotmail.com>
2019-07-14 11:47:43 +0200

    start lesson9 Band Scale

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 //Seleccionamos el elemento svg
 const svg = d3.select('svg');
 
-d3.json('edificios.json').then(datos => {
+d3.json('comida.json').then(datos => {
   //Añadimos ldatos a los rectangulos
 
   const yScale = d3
     .scaleLinear()
-    .domain([0, 1000])
-    .range([0, 500]);
-
+    .domain([0, 250])
+    .range([0, 200]);
   // Band Scale
+  const xScale = d3
+    .scaleBand()
+    .domain(datos.map(item => item.nombre))
+    .range([0, 500]);
 
   const rects = svg.selectAll('rect').data(datos);
 
@@ -28,6 +31,4 @@ d3.json('edificios.json').then(datos => {
     .attr('height', d => yScale(d.altura))
     .attr('fill', 'crimson')
     .attr('x', (d, i) => i * 30);
-
-  console.log(datos);
 });
